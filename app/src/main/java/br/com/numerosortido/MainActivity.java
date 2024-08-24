928eb590f6e47d683ccd376e0c3ba02242f36799
Hugo Eiji <101823718+HGEiji@users.noreply.github.com>
2024-08-24 11:23:31 -0300

    app

---
+++ app/src/main/java/br/com/numerosortido/MainActivity.java
@@ -0,0 +1,38 @@
+package br.com.numerosortido;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void sortearNumero(View view){
+        TextView resultado1 = findViewById(R.id.txtResult);
+
+        resultado1.setText("55");
+
+    }
+
+    public void resetTxt(View view){
+        TextView limpar1 = findViewById(R.id.txtResult);
+
+        limpar1.setText(" ");
+    }
+}
